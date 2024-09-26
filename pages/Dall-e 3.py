9606b29c4ceecbc33c18664bc76681dbb3106c92
lysabouslene <LYSA.BOUSLENE@EEMI.COM>
2024-09-26 15:27:46 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -5,3 +5,5 @@ st.write("Veuillez entré une description de l'image que vous souhaitez generer"
 # Champ de saisi 
 user_input = st.text_input(" ")
 st.write(user_input)
+# sidebare 
+st.sidebar.title("Assistant")
