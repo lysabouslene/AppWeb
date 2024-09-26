32de4d3f86f7264eeb99d28bfa041d85c39d34d9
lysabouslene <LYSA.BOUSLENE@EEMI.COM>
2024-09-26 15:26:15 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -3,5 +3,5 @@ st.title("dall-e 3")
 # Texte 
 st.write("Veuillez entré une description de l'image que vous souhaitez generer")
 # Champ de saisi 
-user_input = st.text_input("Tapez votre texte : ")
+user_input = st.text_input(" ")
 st.write(user_input)
