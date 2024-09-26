adaa57b7ccb64457ac30e0d1c720ba7882a92be0
lysabouslene <LYSA.BOUSLENE@EEMI.COM>
2024-09-26 14:42:37 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -16,7 +16,7 @@ st.sidebar.image("https://th.bing.com/th/id/OIP.QU4hu4jfXbTgoFGCWX1VgAHaC3?rs=1&
 student_grad = st.selectbox("Selectionnez votre niveau d'etude",["Bac","Bac+2","Bac+5"])
 # select slider 
 age = st.select_slider("Quel est votre age ?", range(0, 99))
-if age > 18: 
+if age >= 18: 
   st.write("Vous etes majeur")
 else:
   st.write("Vous n'etes pas majeur")
