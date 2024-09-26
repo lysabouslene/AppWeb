9c47b1e4d5a69b932a5bb9e14b9d6288d3ab5ffe
lysabouslene <LYSA.BOUSLENE@EEMI.COM>
2024-09-26 14:39:03 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -14,3 +14,5 @@ st.sidebar.title("Lysa Bouslene")
 st.sidebar.image("https://th.bing.com/th/id/OIP.QU4hu4jfXbTgoFGCWX1VgAHaC3?rs=1&pid=ImgDetMain")
 # Select Bar
 student_grad = st.selectbox("Selectionnez votre niveau d'etude",["Bac","Bac+2","Bac+5"])
+# select slider 
+age = st.select_slider("Quel est votre age ?", range(0, 99))
